d348e5278c567a390b4e55b3831099cb60c5d6a7
Mario Alexis Juarez Anguiano <anguiano.mario161003@gmail.com>
2026-06-24 13:11:57 -0600

    Agregar entidades y estados de transaccion

---
+++ processor-service/src/main/java/com/fastpay/neowallet/processor_service/entity/Transaction.java
@@ -0,0 +1,57 @@
+package com.fastpay.neowallet.processor_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sender_id")
+    private Long senderId;
+
+    @Column(name = "receiver_id")
+    private Long receiverId;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TransactionStatus status;
+
+    @Column(name = "error_message")
+    private String errorMessage;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (status == null) {
+            status = TransactionStatus.PENDING;
+        }
+
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ processor-service/src/main/java/com/fastpay/neowallet/processor_service/entity/TransactionStatus.java
@@ -0,0 +1,9 @@
+package com.fastpay.neowallet.processor_service.entity;
+
+public enum TransactionStatus {
+    PENDING,
+    DEBITED,
+    COMPLETED,
+    FAILED,
+    ROLLED_BACK
+}
